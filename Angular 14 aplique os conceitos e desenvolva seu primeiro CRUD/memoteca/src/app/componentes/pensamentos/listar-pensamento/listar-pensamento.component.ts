dc4085b04904295d3170ce6d1de5939172f60856
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-04-01 16:17:29 -0300

    Comunicação com Backend

--- Angular 14 aplique os conceitos e desenvolva seu primeiro CRUD/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ Angular 14 aplique os conceitos e desenvolva seu primeiro CRUD/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,24 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente Filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
-      autoria: 'Pensamento',
-      modelo: 'modelo1'
-    }
-
-  ];
+  listaPensamentos: Pensamento [] = [];
   constructor() { }
 
   ngOnInit(): void {
--- Angular 14 aplique os conceitos e desenvolva seu primeiro CRUD/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ Angular 14 aplique os conceitos e desenvolva seu primeiro CRUD/memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-pensamento',
@@ -8,7 +9,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PensamentoComponent implements OnInit {
 
   //Faz o componente receber comunicação do componente pai(listar-pensamento)
-  @Input() pensamento = {
+  @Input() pensamento: Pensamento = {
+    id: 0,
     conteudo: 'I love Angular',
     autoria: 'Paulo',
     modelo: 'modelo3'
