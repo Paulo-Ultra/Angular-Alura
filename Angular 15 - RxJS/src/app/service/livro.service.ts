f8ed2eec60dc59291614fd5e7a6be0b5c851763f
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2024-06-14 09:13:05 -0300

    Otimização da busca com operadores RxJS

--- Angular 15 - RxJS/src/app/service/livro.service.ts
+++ Angular 15 - RxJS/src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
+
 import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
@@ -8,15 +9,15 @@ import { Item, LivrosResultado } from '../models/interfaces';
 })
 export class LivroService {
 
-  private readonly API = 'https://www.googleapis.com/books/v1/volumes'
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes';
   constructor(private http: HttpClient) { }
 
   buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado )
     return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
+      //tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
       map(resultado => resultado.items),
-      tap(resultado => console.log('Fluxo após o map', resultado))
+     //tap(resultado => console.log('Fluxo após o map', resultado))
     )
   }
 }
--- Angular 15 - RxJS/src/app/views/lista-livros/lista-livros.component.ts
+++ Angular 15 - RxJS/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,41 +1,36 @@
-import { Subscription } from 'rxjs';
-import { Component, OnDestroy } from '@angular/core';
-import { LivroService } from 'src/app/service/livro.service';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
 
+const PAUSA = 300;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
-  campoBusca: string = ''
-  subscription: Subscription
-  livro: Livro
+  campoBusca = new FormControl;
 
   constructor(private service: LivroService) { }
 
-  buscarLivros() {
-    this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
-      },
-      error: erro => console.error(erro),
-    }
-    )
-  }
+  //Variável que representa um Observable tem um $ no fim do seu nome(convenção)
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo Inicial')),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map((items) => this.livrosResultadoParaLivros(items))
+  )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
       return new LivroVolumeInfo(item)
     })
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe()
   }
 }
 
